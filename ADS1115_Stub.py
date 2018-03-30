5d3351e157e3ab43ebdc145d0b9aed5d670370cd
Phillia <37705707+psteine3@users.noreply.github.com>
2018-03-30 01:35:29 -0700

    Created ADS1115_Stub.py to repo

---
+++ ADS1115_Stub.py
@@ -0,0 +1,11 @@
+
+
+import smbus # Package for I2C Comms
+
+# Keep commented out unless needed
+# import time
+# import math
+
+
+# Note: The ADS1115 should be run in continuous conversion mode - starts new converstion as soon as the previous data is read
+
