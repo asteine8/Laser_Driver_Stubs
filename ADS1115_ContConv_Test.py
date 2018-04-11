7f4967ee118e55b5df88185796db3eb35a6d98a2
LaserDriverPi <psteine3@gmail.com>
2018-04-11 15:36:37 -0700

    ADS1115 stub vII and continuous conversion over a differential input work and have been tested

---
+++ ADS1115_ContConv_Test.py
@@ -0,0 +1,41 @@
+
+import Adafruit_ADS1x15
+import time
+
+ADS1115_ADDRESS = 0x49 # Current I2C Address of the ADS1115 (find using "sudo i2cdetect -y 1")
+# gain = 1 # FSR = +- 4.096 V
+# gain = 4 # FSR = +- 1.024 V
+gain = 16 # FSR = +- 0.256 V
+dataRate = 860
+
+def ConvertToVoltage(reading, adcGain):
+    if adcGain == 1:
+        return reading * 0.000125
+    elif adcGain == 2:
+        return reading * 0.0000625
+    elif adcGain == 4:
+        return reading * 0.00003125
+    elif adcGain == 8:
+        return reading * 0.000015625
+    elif adcGain == 16:
+	return reading * 0.0000078125
+
+
+ADS1115 = Adafruit_ADS1x15.ADS1115(address=ADS1115_ADDRESS) # Declare ADS1115 as our adc object
+
+ADS1115.start_adc_difference(0,gain,dataRate) # Initialize ADC in continuous conversion mode 
+# ADC channels started in a differential input channel 0 minus channel 1
+
+while 1:
+    runSum = 0 # Preallocate running sum variable
+    numSamples = 50
+    for i in range(numSamples): # iterate for each sample
+	# data = ADS1115.get_last_result()
+	# runSum += data
+	# print(data)
+	runSum += ADS1115.get_last_result() # Sum with aquired data
+
+    aveVolts = ConvertToVoltage(runSum/numSamples, gain) # Get average voltage
+    print(aveVolts) # Output average voltage to console
+    time.sleep(0.2) # Wait a bit to prevent a stack overflow with the print function
+
--- ADS1115_Stub_VII.py
+++ ADS1115_Stub_VII.py
@@ -6,16 +6,16 @@ ADS1115_ADDRESS = 0x49
 gain = 1
 dataRate = 860
 
-ADS1115 = Adafruit_ADS1x15.ADS1115(address=ADS1115_ADDRESS, bus=1) # Declare ADS1115 as our adc object
+ADS1115 = Adafruit_ADS1x15.ADS1115(address=ADS1115_ADDRESS) # Declare ADS1115 as our adc object
 
-ADS1115.start_adc(0,gain,dataRate) # Initialize ADC in continuous conversion mode
+ADS1115.start_adc(0,gain,dataRate) # Initialize ADC in continuous conversion mode measuring off channel 1
 
-adcData = []
-for i in range(1000):
+adcData = range(100)
+for i in range(100):
     adcData[i] = ADS1115.get_last_result() # Gets latest result stored in ADC register
-    time.sleep(1/860) # Wait a bit (No point in going faster...
+    time.sleep(0.05) # Wait a bit (No point in going faster...
 
-for i in range(1000):
+for i in range(100):
     print(adcData[i])
 
 
